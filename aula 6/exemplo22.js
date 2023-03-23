e71978446f878b343fc105ef83a15fa05e65e09a
aloisio <aloisio-jorge@gmail.com>
2023-03-23 10:31:46 -0300

    exemplo 22

---
+++ aula 6/exemplo22.js
@@ -0,0 +1,16 @@
+class Operacoes{
+    constructor(){
+
+    }
+
+    somar(valorA, valorB){
+        console.log(valorA + valorB)
+    }
+
+    somar(valorA, valorB, valorC){
+        consolelog(valorA + valorB + valorC)
+    }
+}
+
+var objeto = new Operacoes()
+objeto.somar(10,20,30)
