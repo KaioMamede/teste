9e84490e4daac0f1862fad75f7a9b8c2eacbeeb9
aloisio <aloisio-jorge@gmail.com>
2023-04-06 09:24:01 -0300

    novo projeto

---
+++ aula 07/projetonode/app.js
@@ -0,0 +1,15 @@
+//fazer a importação do express
+const express = require('express');
+
+//Rotas (quando o servidor for acessado, para onde ele vai?)
+const router = express.Router();
+router.get('/',(req,res)=>{
+    res.send('olá Mundo!!');
+});
+
+//configurações basicas do appp
+const app=express();
+app.use('/',router);//passamos apenas uma rota, pois foi criada 1
+
+module.exports = app;//exportamos o app pois iremos usa-lo no servidor
+
