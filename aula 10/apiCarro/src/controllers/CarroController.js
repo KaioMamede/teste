96a9c3780a861ff8c583110bb119b2a3e1aa6306
aloisio <aloisio-jorge@gmail.com>
2023-05-11 10:40:23 -0300

    novos exemplos

--- aula 10/apiCarro/src/controllers/CarroController.js
+++ aula 10/apiCarro/src/controllers/CarroController.js
@@ -1,1 +1,17 @@
 const CarroServices = require('../services/CarroServices');
+module.exports = {
+     buscarTodos: async(req, res) =>{
+        let json = {error:'', result:[]};
+
+        let carros = await CarroService.buscarTodos();
+
+        for(let i in carros){
+            json.result.push({
+                codigo: carros[i].codigo,
+                descricao: carros[i].modelo,
+                placa: carros[i].placa
+            });
+        }
+        res.json(json);
+    }
+}
--- aula 10/apiCarro/src/services/CarroServices.js
+++ aula 10/apiCarro/src/services/CarroServices.js
@@ -1,3 +1,13 @@
+const CarroService = require('../db');
+
 module.exports = {
     
-}
+    buscarTodos: () =>{
+        return new Promise((aceito, rejeitado)=>{
+            db.query('SELECT * FROM carros', (error, results)=>{
+                if(error) { rejeitado(error); return;}
+                aceito(results);
+            });
+        });
+    }
+};
