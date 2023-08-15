0418cbbc46f15ad208c1710aea0df48020a91a8a
Auto Logon <autologon@CSED.LAB>
2023-08-15 09:55:55 -0300

    Mercado

---
+++ Mercado/js/script.js
@@ -0,0 +1,25 @@
+const btnMobile = document.getElementById('btn-mobile');
+const links = document.querySelectorAll('.link')
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+links.forEach(element => {
+  element.addEventListener("click", ()=>{
+    const nav = document.getElementById('nav');
+    nav.classList.remove("active");
+  });
+});
