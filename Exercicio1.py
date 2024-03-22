0eb95822142d7af89f8d0e78f993522bc4a54f3c
KaioMamede <127403387+KaioMamede@users.noreply.github.com>
2024-03-22 15:07:53 -0300

    Delete Exercicio1.py

--- Exercicio1.py
+++
@@ -1,7 +0,0 @@
-print ("Ciencia da computação ")
-
-prof = input("Digitte sua profissão: ")
-print ("Sua profissãi é: " + prof)
-
-idade = input ("Digite sua idade: ")
-print ("Sua idade é: " + idade)
