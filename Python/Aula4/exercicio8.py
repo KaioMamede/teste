816502d54c44e35cd08087d6892a1f6e78cbffac
KaioMamede <127403387+KaioMamede@users.noreply.github.com>
2024-03-26 10:49:16 -0300

    Delete Python/Aula4/exercicio8.py

--- Python/Aula4/exercicio8.py
+++
@@ -1,8 +0,0 @@
-#faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo
-
-n1 = float(input("Digite um numero:  "))
-
-if n1 >=0:
-    print("O numero é positivo")
-else:
-    print("O numero é negativo!")
